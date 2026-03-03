6afe9315b2ba43391258b1682b40c5602542110f
varxnn7 <varunkukreja010@gmail.com>
2026-03-04 03:04:23 +0530

    Cover React-Element, React Components, Component Compositiona and Javascript inside JSX and all the possible cases

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Object => HTMLElement(rendor)
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-console.log(heading);
+const elem = <span> React Element</span>
+//React Element
+const Heading = (<h1 className='head'>
+    Namaste React (using JSX)
+    {elem}  {/* React element inside React element */}
+</h1>
+);
 
 
-// JSX (transpiled before it reaches Js Engine) - Parcel - Babel
+// React Component
+const Title = () => {
+    return <h1 className="head" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+};
+// Class Based Component - OLD
+// Functional Component - NEW
 
-// JSX => React.createElement => ReactElement-JS Object => HTMLElement(rendor)
 
-const jsxHeading = (<h1 className='head'>
-    Namaste React using JSX
-</h1>)
-console.log(jsxHeading);
+// React Functional Component 
+
+const HeadingComponent = () => (
+     <h1 className="heading"> Namaste React Functional Component</h1>
+);
+// Component Composition
+const HeadingComponent2 = () => (
+     <div id="container">
+        {Title()}   {/* calling function or Functional Component in JSX */}
+        {100+200}   {/* Javascript inside JSX (it uses cross side scripting attack hence it is secured)*/}  
+        {Heading}   {/* React Element inside JSX */}
+        <Title />   {/* FunctionalComponent inside JSX */}
+        <h1 className="heading"> Namaste React Functional Component</h1>
+    </div>
+); 
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading);
+root.render(<HeadingComponent2 />);
