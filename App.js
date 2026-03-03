d17bb30746fd50b36e9a7e90372bf1b04c154e97
varxnn7 <varunkukreja010@gmail.com>
2026-03-03 15:49:59 +0530

    simple program using react by wrting react code in seperate file and applying some css

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+    "h1",
+    { id: 'heading', xyz: "abc" },
+    "hello world from react!"
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
