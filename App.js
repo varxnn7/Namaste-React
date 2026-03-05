99e260b85cd6d6b30b328b08eac42c442bb2f80b
varxnn7 <varunkukreja010@gmail.com>
2026-03-05 15:35:41 +0530

    Food Delivery App - just make header, body and footer component

--- App.js
+++ App.js
@@ -1,41 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem = <span> React Element</span>
-//React Element
-const Heading = (<h1 className='head'>
-    Namaste React (using JSX)
-    {elem}  {/* React element inside React element */}
-</h1>
-);
+ /** 
+  * Header
+  *  - Logo
+  *  - Nav Items
+  * Body
+  *  - Search 
+  *  - Restaurant Container
+  *      - Restaurant Card
+  *         - Image
+  *         - Name of res star rating , cuisene, delivery time
+  * Footer
+  *  - Copyright
+  *  - Links
+  *  - Address
+  *  - Contact
+  */
 
-
-// React Component
-const Title = () => {
-    return <h1 className="head" tabIndex="5">
-        Namaste React using JSX
-    </h1>
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src ="https://thumbs.dreamstime.com/b/food-burger-logo-vector-emblem-design-template-128946515.jpg" alt="logo"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>       
+            </div>
+        </div>
+    )
 };
-// Class Based Component - OLD
-// Functional Component - NEW
+
+const RestaurantCard = () => {
+    return (
+
+        <div className = "res-card" style={{ backgroundColor: "#f0f0f0"}}>
+            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/RX_THUMBNAIL/IMAGES/VENDOR/2024/8/21/6031633c-08bb-4a8b-9af9-70522c355308_353197.jpg"/>
+        <h3> Meghna foods</h3>
+            <h4> Biryani, North indian , Asian </h4>
+            <h4> 4.4 stars </h4>
+            <h4> 38 minutes </h4> 
+        </div>
+    );
+};
 
 
-// React Functional Component 
+const Body = () => {
+    return (
+        <div className= "body">
+            <div className="search"> Search </div>
+            <div className= "res-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                
+            </div>
+        </div>
 
-const HeadingComponent = () => (
-     <h1 className="heading"> Namaste React Functional Component</h1>
-);
-// Component Composition
-const HeadingComponent2 = () => (
-     <div id="container">
-        {Title()}   {/* calling function or Functional Component in JSX */}
-        {100+200}   {/* Javascript inside JSX (it uses cross side scripting attack hence it is secured)*/}  
-        {Heading}   {/* React Element inside JSX */}
-        <Title />   {/* FunctionalComponent inside JSX */}
-        <h1 className="heading"> Namaste React Functional Component</h1>
-    </div>
-); 
+    );
+};
 
+ const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+ } 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent2 />);
+root.render(<AppLayout />);
