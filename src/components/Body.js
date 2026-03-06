694b9f687bbaaa93158edf256f288ccc450b123b
varxnn7 <varunkukreja010@gmail.com>
2026-03-06 15:11:57 +0530

    restructure the project and learn hook in react by using usestate and useeffect and also learn Reconciliation Algo and Diffing Algo and core concept of Virtual DOM

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  // local state varaiable - super powerful variable
   const restaurants =
     resList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
-
+  const [ListOfRestaurants, setListOfRestaurants] = useState(restaurants);
   return (
     <div className="body">
+      <div className= "filter">
+        <button 
+          className="filter-btn"
+          onClick={() => { 
+            const filteredList = ListOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+
+        </button>
+      </div>
       <div className="res-container">
-        {restaurants.map((restaurant) => (
+        {ListOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             resData={restaurant}
