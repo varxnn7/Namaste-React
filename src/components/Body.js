48bad9e99ec445e99821aa902606c930438849a8
varxnn7 <varunkukreja010@gmail.com>
2026-03-06 16:40:32 +0530

    implemented Microservice Architecture and used useEffect in and also implemented shimmer effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,36 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   // local state varaiable - super powerful variable
   const restaurants =
     resList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
-  const [ListOfRestaurants, setListOfRestaurants] = useState(restaurants);
+  const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+
+ // this is been called after the component is rendered on the screen
+  useEffect(() => {
+    // console.log("called");
+    fetchData();
+  }, []);
+  // this will be called first 
+  // console.log("Body rendered");
+
+const fetchData = async () => {
+  const data = await fetch(
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.190473&lng=79.926989&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+);
+  const json = await data.json();
+  console.log(json);
+  // optional chaining
+  setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+};
+
+if(ListOfRestaurants.length === 0){
+  return <Shimmer />;
+}
+ 
   return (
     <div className="body">
       <div className= "filter">
